19b7b807805bd5d6e8c6c43e15ad50af818cf4d9
kusabuka-yuki <yk.ksbk0211@gmail.com>
2024-09-07 14:46:05 +0900

    add

---
+++ Libraries/AngleSharpLoader/AngleSharpLoader.cs
@@ -0,0 +1,22 @@
+﻿using AngleSharp;
+
+namespace AngleSharpLoader
+{
+    public class AngleSharpLoader
+    {
+        public string Address { get; set; }
+        private IConfiguration config;
+        public IBrowsingContext Context
+        {
+            get
+            {
+                return BrowsingContext.New(config);
+            }
+        }
+        public AngleSharpLoader(string address)
+        {
+            config = Configuration.Default.WithDefaultLoader();
+            Address = address;
+        }
+    }
+}
